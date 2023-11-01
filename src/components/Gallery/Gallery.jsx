8af1bae2f9c24ceea10d0bf347194a5c51c887f1
unknown <muttakinahmed871@gmail.com>
2023-11-01 15:50:20 +0600

    showed images and made responsive

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,7 @@
 import "./Gallery.css";
 const Gallery = () => {
   const images = [
+    "/images/image-11.jpeg",
     "/images/image-1.webp",
     "/images/image-2.webp",
     "/images/image-3.webp",
@@ -10,12 +11,15 @@ const Gallery = () => {
     "/images/image-7.webp",
     "/images/image-8.webp",
     "/images/image-9.webp",
+    "/images/image-10.jpeg",
   ];
   return (
     <div className="container">
       {images.map((image, index) => (
-        <div key={index} className={`image ${index === 0 ? "large" : "small"}`}>
-          <img src={image} alt="" />
+        <div
+          className={`image-container ${index === 0 ? "first-image" : ""}`}
+          key={index}>
+          <img className="image" src={image} alt="" />
         </div>
       ))}
     </div>
