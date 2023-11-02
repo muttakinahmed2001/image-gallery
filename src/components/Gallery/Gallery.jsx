f4a87f94416b5a8cce38f1a5aae4619b2f9a9ae2
unknown <muttakinahmed871@gmail.com>
2023-11-02 22:52:36 +0600

    dispalyed checkbox after mouse entering and clicking

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
 import "./Gallery.css";
+
 const Gallery = () => {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [clickedImage, setClickedImage] = useState([]);
+  console.log(selectedImages, "this image hovered");
+  console.log(clickedImage, "this image clicked");
+
+  const handleImageHover = (index) => {
+    if (!clickedImage.includes(index)) {
+      setSelectedImages([...selectedImages, index]);
+    }
+  };
+
+  const handleImageLeave = (index) => {
+    setSelectedImages(selectedImages.filter((i) => i !== index));
+  };
+  const handleImageClicked = (index) => {
+    if (clickedImage.includes(index)) {
+      setClickedImage(clickedImage.filter((i) => i !== index));
+    } else {
+      setClickedImage([...clickedImage, index]);
+      setSelectedImages(selectedImages.filter((i) => i !== index));
+    }
+  };
+
   const images = [
     "/images/image-11.jpeg",
     "/images/image-1.webp",
@@ -13,13 +38,30 @@ const Gallery = () => {
     "/images/image-9.webp",
     "/images/image-10.jpeg",
   ];
+
   return (
     <div className="container">
       {images.map((image, index) => (
         <div
           className={`image-container ${index === 0 ? "first-image" : ""}`}
           key={index}>
-          <img className="image" src={image} alt="" />
+          <img
+            className="image"
+            src={image}
+            alt=""
+            onMouseEnter={() => handleImageHover(index)}
+            onMouseLeave={() => handleImageLeave(index)}
+            onClick={() => handleImageClicked(index)}
+          />
+          {selectedImages.includes(index) ? (
+            <div className="blank-box"></div>
+          ) : clickedImage.includes(index) ? (
+            <div>
+              <h2 className="blank-box">&#10003;</h2> {/* Checkmark (✓) */}
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       ))}
     </div>
